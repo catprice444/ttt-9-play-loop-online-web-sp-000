f4ab44943a0333a1b95dc7ae6f4f5b415321b033
Catherine Price <catprice444@gmail.com>
2020-04-10 22:42:07 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,8 +36,8 @@ def turn(board)
 end
 
 # Define your play method below
-def play(board,turn)
-  counter=0
-  while counter < 9
-
-end
+# def play(board,turn)
+#   counter=0
+#   while counter < 9
+#
+# end
